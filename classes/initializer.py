decf1f3c11122c6d4178b2daad3a8772cebd73ab
Cynthia Mugo <cynthiamugo25@gmail.com>
2025-08-07 11:05:30 +0300

    self initializer

--- classes/initializer.py
+++ classes/initializer.py
@@ -7,13 +7,36 @@
 # method you create has to have the self keyword
 
 class Human():
-    def __init__(self):
+    # self references the object inside the class
+    def __init__(self, gender, name):
         print("The initializer was called")
-    def another_one(self):
+        # print(gender)
+        # print(name)
+        self.gender = gender
+        self.name = name
+        if self.gender == "Male":
+            self.ribs = 24
+            self.curse = "Suffer"
+        else :
+            self.ribs = 23
+            self.curse = "Pain"
+
+    def another_one(self, gender, name):
         print("This is another method")
 
 # __init__ is called automatically but any other methods inside the class you have to call them yourself
-adam = Human() # object from a class
+adam = Human(name="Adam", gender="Male") # object from a class
 # adam.__init__()
 # adam.__init__()
-adam.another_one()
+# adam.another_one()
+print("name", adam.name)
+print("gender", adam.gender)
+print("ribs", adam.ribs)
+print("curse", adam.curse)
+print("")
+
+eve = Human(name="Eve", gender="female")
+print("name", eve.name)
+print("gender", eve.gender)
+print("ribs", eve.ribs)
+print("curse", eve.curse)
