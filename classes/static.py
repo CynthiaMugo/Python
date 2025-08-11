3c03a888bd980a0e6285b941e9a09e4877a937dc
Cynthia Mugo <cynthiamugo25@gmail.com>
2025-08-11 09:47:33 +0300

    class methods

--- classes/static.py
+++ classes/static.py
@@ -14,14 +14,14 @@ class Human():
     
     def __init__(self,gender,name):
         print("The initializer was called")
-        self._gender=gender
-        self._name=name
-        if self._gender=="Male":
-            self._ribs=24
-            self._curse="Suffer"
+        self._gender = gender
+        self._name = name
+        if self._gender == "Male":
+            self._ribs = 24
+            self._curse = "Suffer"
         else :
-          self._ribs=23
-          self._curse="Pain"
+          self._ribs = 23
+          self._curse = "Pain"
 
         # Human.count = Human.count + 1
         # in case of changes in class name use self:
@@ -32,7 +32,7 @@ class Human():
     def name(self):
         now = datetime.now()
         print("Current date and time",now)
-        write_file(f_name="log.txt",txt=f"At {now} got name from adam")
+        write_file(f_name = "log.txt", txt = f"At {now} got name from adam")
         return self._name
     
     @name.setter
@@ -56,15 +56,24 @@ class Human():
         print("curse",self._curse)
         print("---------------------")
 
+    @classmethod
+    def get_general_info(cls):
+        print("Species", cls.species) #also can be called as Human.species to call to the static properties
+        print("Species", cls.genus)
+        print("Species", cls.count)
+
 
 # adam=Human(name="adam",gender="Male") #object from a class
-adam = Human(name="adam", gender="Male")
-eve = Human(name="eve", gender="Female")
-cain = Human(name="cain", gender="Male")
+# adam = Human(name="adam", gender="Male")
+# eve = Human(name="eve", gender="Female")
+# cain = Human(name="cain", gender="Male")
 
-cain.print_self()
-print("adam species", adam.species)
-print("eve species", eve.species)
-print("class property", Human.species)
+# cain.print_self()                                                                                   
+# print("adam species", adam.species)
+# print("eve species", eve.species)
+# print("class property", Human.species)
 
-print("Total count is", Human.count)
+# print("Total count is", Human.count)
+
+# Calling static method - refers to method pertaining the class itself
+Human.get_general_info()
