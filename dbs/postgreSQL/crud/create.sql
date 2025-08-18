cdeab67ef3710d98e489f0a9148e29bcd6e876e5
Cynthia Mugo <cynthiamugo25@gmail.com>
2025-08-18 13:08:24 +0300

    Update student table constraints and add test data scripts

--- dbs/postgreSQL/crud/create.sql
+++ dbs/postgreSQL/crud/create.sql
@@ -1,7 +1,9 @@
 INSERT INTO student
 (name,email,phone,pocket_money)
 VALUES
--- ('John Doe', 'johndoe@gmail.com', 0722045678, 500),
--- ('Delilah Samson', 'delilahsamson@gmail.com', 0722678902, 1500)
+('John Doe', 'johndoe@gmail.com', 0722045678, 500),
+('Delilah Samson', 'delilahsamson@gmail.com', 0722678902, 1500)
+
+
 
 
---
+++ dbs/postgreSQL/crud/testdata_mutate.sql
@@ -0,0 +1,20 @@
+-- SELECT * FROM student
+-- WHERE marks >= 80
+-- ORDER BY marks DESC
+
+-- ALTER TABLE student 
+-- add
+-- pocket_money INTEGER;
+
+-- ALTER TABLE student drop
+-- COLUMN is_married;
+
+-- UPDATE student
+-- SET
+-- pocket_money = 2000
+-- WHERE marks >= 95;
+
+SELECT name, marks, pocket_money FROM student
+WHERE marks < 50
+ORDER BY pocket_money DESC
+LIMIT 15;
