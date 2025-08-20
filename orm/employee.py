82a9a2731ef5a1eaafe6eba7a1458422617103d6
Cynthia Mugo <cynthiamugo25@gmail.com>
2025-08-20 15:55:04 +0300

    Implement employee retrieval method

--- orm/employee.py
+++ orm/employee.py
@@ -5,7 +5,7 @@ class Employee:
     def __init__(self):
         # print("Initializing Employee class")
         pg = PG()
-        self.pg = pg
+        # self.pg = pg
         query="""
             CREATE TABLE IF NOT EXISTS employee(
               id BIGSERIAL PRIMARY KEY,
@@ -22,6 +22,37 @@ class Employee:
             VALUES (%s, %s)
         """
         pg.pg_query(query=query, params=(name, email))
+    def all(self):
+        pg=PG()
+        query="""
+          SELECT * FROM employee
+        """
+        results=pg.pg_query(query=query)
+        #print(results)
+        for employee in results:
+            print(employee)
+        return
 
 emp1 = Employee()
-emp1.add(name='Joan Sloan', email='sloan@gmail.com')
+# emp1.add(name='Joan Sloan', email='sloan@gmail.com')
+
+# name = input("Enter employee name: ")
+# email = input("Enter employee email: ")
+# emp1.add(name=name, email=email)
+# print("Employee added successfully.")
+
+# Adding with a loop to add multiple employees
+# while True:
+#     name=input("Enter employee name:")
+#     email=input("Enter employee email:")
+#     emp1.add(name=name,email=email)
+
+#     print("Employee added ")
+#     print("")
+#     another=input("Enter y to add another ?:")
+#     if another=="y" or another=="Y":
+#         continue
+#     break
+# print("Exiting the employee addition process.")
+  
+emp1.all()  # Display all employees
